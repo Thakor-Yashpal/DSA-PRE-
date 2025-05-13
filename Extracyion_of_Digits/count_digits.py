f6fbbbfbe4116eaeaa3a733aa3fad6c517bc80dd
Thakor-Yashpal <thakoryashpal755@gmail.com>
2025-05-13 19:42:32 +0530

    new example

--- Extracyion_of_Digits/count_digits.py
+++ Extracyion_of_Digits/count_digits.py
@@ -29,4 +29,20 @@ NUm01 = N1
 while NUm01 > 0:
     last_numbers = NUm01 % 10
     NUm01 = NUm01 // 10
-    print(last_numbers)
+    print(f"{last_numbers} is the last number in the number")
+    
+ 
+
+
+# Example 03
+
+N1 = 23481  # Initial number
+count = 0   # Counter to track the number of digits
+NUm02 = N1  # Copy of N1 to manipulate
+
+# Loop to count digits and extract last digit in each iteration
+while NUm02 > 0:
+    count += 1
+    last_digit = NUm02 % 10  # Get the last digit
+    NUm02 = NUm02 // 10      # Remove the last digit
+    print("Count value:", count, "Last digit:", last_digit)
