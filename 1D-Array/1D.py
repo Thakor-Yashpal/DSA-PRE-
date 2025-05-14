91cbe4835e5477209f87512e283acfe06ea366da
Thakor-Yashpal <thakoryashpal755@gmail.com>
2025-05-14 21:11:49 +0530

    new code to add

--- 1D-Array/1D.py
+++ 1D-Array/1D.py
@@ -2,9 +2,7 @@ class Solution(object):
     def mergeAlternately(self, word1, word2):
         self.word1 = "abc"
         self.word2 = "pqr"
-        range = len(word1)
-        range1 = len(word2)
-        if word1 >= word2:
-                print(word1)
-        else:
-                print(word2)
+    for w in range(len(word1)):
+        for w1 in range(len(word2)):
+            if word1 != word2:
+                append.word
--- Extracyion_of_Digits/Armstorng_number.py
+++ Extracyion_of_Digits/Armstorng_number.py
@@ -81,13 +81,3 @@ else:
 # Space Complexity: O(1)
 # - Only a fixed number of variables are used regardless of input size
 # -----------------------------------------
-class Solution(object):
-    def mergeAlternately(self, word1, word2):
-        self.word1 = "abc"
-        self.word2 = "pqr"
-        range = len(word1)
-        range1 = len(word2)
-        if word1 >= word2:
-                print(word1)
-        else:
-                print(word2)
